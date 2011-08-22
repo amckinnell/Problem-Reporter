fa490b82fb90ff7c15feb8b51bce161b6adb7ec1
Alistair McKinnell <alistair.mckinnell@gmail.com>
2011-08-22 09:56:26 -0400

    Use the freshly created valuablecode.public Gmail configuration.

--- src/main/java/com/valuablecode/GmailSmtpConfiguration.java
+++ src/main/java/com/valuablecode/GmailSmtpConfiguration.java
@@ -26,7 +26,7 @@ class GmailSmtpConfiguration implements EmailReportServiceConfiguration {
     public String getEmailAddressForAdministrator() {
         if (null == emailAddressForAdministrator) {
             emailAddressForAdministrator = System.getProperty("administrator.email.address",
-                    "alistair.mckinnell@gmail.com");
+                    "valuablecode.public@gmail.com");
         }
 
         return emailAddressForAdministrator;
@@ -63,7 +63,7 @@ class GmailSmtpConfiguration implements EmailReportServiceConfiguration {
 
     public String getUser() {
         if (null == user) {
-            user = System.getProperty("gmail.smtp.user", "alistair.mckinnell@gmail.com");
+            user = System.getProperty("gmail.smtp.user", "valuablecode.public@gmail.com");
         }
 
         return user;
